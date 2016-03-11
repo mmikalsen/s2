7193def92c8b5aa99692084a1d4d550d930e2140
Kristoffer Severinsen <kristoffer.severinsen@gmail.com>
2016-03-11 11:59:45 +0100

    activated delay on backend/compute

--- src/greencompute.py
+++ src/greencompute.py
@@ -19,29 +19,29 @@ def waitToProduce():
     return tts
 
 class FrontendHttpHandler(BaseHTTPServer.BaseHTTPRequestHandler):
-    
-    # Returns the 
+
+    # Returns the
     def do_GET(self):
-    
-        # print "waiting " + str(waitToProduce(10)).replace('.', ',') + " ms"
-    
+
+        print "waiting " + str(waitToProduce(10)).replace('.', ',') + " ms"
+
         # Write header
         self.send_response(200)
         self.send_header("Content-type", "application/octet-stream")
         self.end_headers()
-        
+
         # Write Body
         self.wfile.write("GREEN")
         self.wfile.close()
-        
+
     def sendErrorResponse(self, code, msg):
         self.send_response(code)
         self.send_header("Content-type", "text/html")
         self.end_headers()
         self.wfile.write(msg)
-        
+
 class FrontendHTTPServer(BaseHTTPServer.HTTPServer):
-    
+
     def server_bind(self):
         BaseHTTPServer.HTTPServer.server_bind(self)
         self.socket.settimeout(2)
@@ -66,13 +66,13 @@ class FrontendHTTPServer(BaseHTTPServer.HTTPServer):
 
 if __name__ == '__main__':
     httpserver_port = 8000
-    
+
     try:
         optlist, args = getopt.getopt(sys.argv[1:], 'x', ['skew=', 'multiplier=', 'port='])
     except getopt.GetoptError:
         print sys.argv[0] + ' [--port portnumber(default=8000)] [--skew value] [--multiplier value]'
         sys.exit(2)
-    
+
     for opt, arg in optlist:
         if opt in ("-skew", "--skew"):
             latencySkew = int(arg)
@@ -80,26 +80,22 @@ if __name__ == '__main__':
             latencyMultiplier = int(arg)
         elif opt in ("-port", "--port"):
             httpserver_port = int(arg)
-    
+
     # Start the webserver which handles incomming requests
     try:
         httpd = FrontendHTTPServer(("",httpserver_port), FrontendHttpHandler)
         server_thread = threading.Thread(target = httpd.serve)
         server_thread.daemon = True
         server_thread.start()
-        
+
         def handler(signum, frame):
             print "Stopping http server..."
             httpd.stop()
         signal.signal(signal.SIGINT, handler)
-        
+
     except:
         print "Error: unable to http server thread"
-        
+
     # Wait for server thread to exit
     server_thread.join(100)
     httpd.stop()
-
-      
-
-
